c7cc54f974fc4ef0eb5ff04c0326a1dc863cf19c
SaulFromRobots <saulrs951@protonmail.com>
2024-10-15 10:47:49 -0700

    install mypy w/ pyproject

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ let
 	fhs = pkgs.buildFHSUserEnv {
 		name = "my-fhs-environment";
 
-		targetPkgs = _: with pkgs; [ python3 xorg.libX11 libGL mypy ];
+		targetPkgs = _: with pkgs; [ python3 xorg.libX11 libGL ];
 
 		profile = ''
 			set -e
