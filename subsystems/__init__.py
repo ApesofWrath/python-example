e7771c0fbf6061b8ff8e5028dc7d5e68d3d2a0fa
Ashwin Naren <arihant2math@gmail.com>
2025-01-18 19:25:39 -0800

    added limelight to fancy swerve

---
+++ subsystems/__init__.py
@@ -0,0 +1,4 @@
+from drivetrain import CommandSwerveDrivetrain as Drivetrain
+from limelight import Limelight, LimelightPose
+from spinner import Spinner
+from turntable import Turntable
--- subsystems/limelight/limelight_pose.py
+++ subsystems/limelight/limelight_pose.py
@@ -19,11 +19,17 @@ class LimelightPose:
     avg_tag_distance: float
     avg_tag_area: float
     update_time: int
+    stddev_x: float
+    stddev_y: float
+    stddev_yaw: float
 
-    def __init__(self, entry: NetworkTableEntry):
+    def __init__(self, entry: NetworkTableEntry, stddev_entry: NetworkTableEntry):
         pose = entry.getDoubleArray(None)
         if pose is None:
             raise AttributeError("Double Array not found as entry value")
+        stddevs = stddev_entry.getDoubleArray(None)
+        if stddevs is None:
+            raise AttributeError("TODO: fill in")
         self.x = pose[0]
         self.y = pose[1]
         self.z = pose[2]
@@ -36,8 +42,17 @@ class LimelightPose:
         self.avg_tag_distance = pose[9]
         self.avg_tag_area = pose[10]
         self.update_time = entry.getLastChange()
+        self.stddev_x = stddevs[6]
+        self.stddev_y = stddevs[7]
+        self.stddev_yaw = stddevs[11]
 
     def time(self):
-        # TODO: Units
+        """
+        Gets the time the limelight pose was measured.
+        :return: time in milliseconds
+        """""
         # TODO: is timestamp the right timestamp
-        return self.update_time - self.latency
+        return self.update_time * 1000.0 - self.latency
+
+    def covariance(self) -> tuple[float, float, float]:
+        return self.stddev_x, self.stddev_y, self.stddev_yaw
